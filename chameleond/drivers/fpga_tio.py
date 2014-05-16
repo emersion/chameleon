0703c54b9efcb621cd93b3a253df4c5b83bf25d5
Tom Wai-Hong Tam <waihong@chromium.org>
2014-05-16 14:13:26 +0800

    Add the Chameleond driver for FPGA TIO (Three-In-One) board
    It is the major piece to put all the previous libraries together into
    a Chameleond driver.
    
    Several methods are still missing. Will be added later.
    
    BUG=chromium:374103
    TEST=Ran display_EdidStress, worked on HDMI and DisplayPort interfaces.
    
    Change-Id: I6220c201c20bc6b16a78209b1bceec46ebf8d8e7
    Reviewed-on: https://chromium-review.googlesource.com/200220
    Reviewed-by: Chih-Chung Chang <chihchung@chromium.org>
    Commit-Queue: Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Wai-Hong Tam <waihong@chromium.org>

---
+++ chameleond/drivers/fpga_tio.py
@@ -0,0 +1,309 @@
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Chameleond Driver for FPGA customized platform with the TIO card."""
+
+import logging
+import os
+import xmlrpclib
+
+import chameleon_common  # pylint: disable=W0611
+from chameleond.interface import ChameleondInterface
+from chameleond.utils import fpga
+from chameleond.utils import i2c_fpga as i2c
+from chameleond.utils import ids
+from chameleond.utils import input_flow
+
+
+class DriverError(Exception):
+  """Exception raised when any error on FPGA driver."""
+  pass
+
+
+class ChameleondDriver(ChameleondInterface):
+  """Chameleond Driver for FPGA customized platform."""
+
+  _I2C_BUS_MAIN = 0
+
+  def __init__(self, *args, **kwargs):
+    super(ChameleondDriver, self).__init__(*args, **kwargs)
+    self._selected_input = None
+    # Reserve index 0 as the default EDID.
+    self._all_edids = [self._ReadDefaultEdid()]
+
+    main_bus = i2c.I2cBus(self._I2C_BUS_MAIN)
+    fpga_ctrl = fpga.FpgaController()
+    self._input_flows = {
+      ids.DP1: input_flow.DpInputFlow(ids.DP1, main_bus, fpga_ctrl),
+      ids.DP2: input_flow.DpInputFlow(ids.DP2, main_bus, fpga_ctrl),
+      ids.HDMI: input_flow.HdmiInputFlow(ids.HDMI, main_bus, fpga_ctrl),
+      ids.VGA: input_flow.VgaInputFlow(ids.VGA, main_bus, fpga_ctrl)
+    }
+
+    for flow in self._input_flows.itervalues():
+      if flow:
+        flow.Initialize()
+
+    self.Reset()
+
+    # Set all ports unplugged on initialization.
+    for input_id in self.ProbeInputs():
+      self.Unplug(input_id)
+
+
+  def Reset(self):
+    """Resets Chameleon board."""
+    logging.info('Execute the reset process.')
+    # TODO(waihong): Add other reset routines.
+    self._ApplyDefaultEdid()
+
+
+  def IsHealthy(self):
+    """Returns if the Chameleon is healthy or any repair is needed.
+
+    Returns:
+      True if the Chameleon is healthy; otherwise, False, need to repair.
+    """
+    # TODO(waihong): Add the check of health when needed.
+    return True
+
+  def Repair(self):
+    """Repairs the Chameleon.
+
+    It can be an asynchronous call, e.g. do the repair after return. An
+    approximate time of the repair is returned. The caller should wait that
+    time before the next action.
+
+    Returns:
+      An approximate repair time in second.
+    """
+    # TODO(waihong): Add the repair routine when needed.
+    return 0
+
+  def GetSupportedInputs(self):
+    """Returns all supported connectors on the board.
+
+    Not like the ProbeInputs() method which only returns the connectors which
+    are connected, this method returns all supported connectors on the board.
+
+    Returns:
+      A tuple of input_id, for all supported connectors on the board.
+    """
+    return self._input_flows.keys()
+
+  def IsPhysicalPlugged(self, input_id):
+    """Returns if the physical cable is plugged.
+
+    It checks the source power +5V/+3.3V pin.
+
+    Args:
+      input_id: The ID of the input connector.
+
+    Returns:
+      True if the physical cable is plugged; otherwise, False.
+    """
+    return self._input_flows[input_id].IsPhysicalPlugged()
+
+  def ProbeInputs(self):
+    """Probes all the display connectors on Chameleon board.
+
+    Returns:
+      A tuple of input_id, for the connectors connected to DUT.
+    """
+    input_ids = []
+    for input_id in self.GetSupportedInputs():
+      if self.IsPhysicalPlugged(input_id):
+        input_ids.append(input_id)
+    return tuple(input_ids)
+
+  def GetConnectorType(self, input_id):
+    """Returns the human readable string for the connector type.
+
+    Args:
+      input_id: The ID of the input connector.
+
+    Returns:
+      A string, like "VGA", "DVI", "HDMI", or "DP".
+    """
+    return self._input_flows[input_id].GetConnectorType()
+
+  def WaitVideoInputStable(self, input_id, timeout=None):
+    """Waits the video input stable or timeout.
+
+    Args:
+      input_id: The ID of the input connector.
+      timeout: The time period to wait for.
+
+    Returns:
+      True if the video input becomes stable within the timeout period;
+      otherwise, False.
+    """
+    # TODO(waihong): Implement this method.
+    return True
+
+  def _ReadDefaultEdid(self):
+    """Reads the default EDID from file.
+
+    Returns:
+      A byte array of EDID data.
+    """
+    driver_dir = os.path.dirname(os.path.realpath(__file__))
+    edid_path = os.path.join(driver_dir, '..', 'data', 'default_edid.bin')
+    return open(edid_path).read()
+
+  def CreateEdid(self, edid):
+    """Creates an internal record of EDID using the given byte array.
+
+    Args:
+      edid: A byte array of EDID data, wrapped in a xmlrpclib.Binary object.
+
+    Returns:
+      An edid_id.
+    """
+    if None in self._all_edids:
+      last = self._all_edids.index(None)
+      self._all_edids[last] = edid.data
+    else:
+      last = len(self._all_edids)
+      self._all_edids.append(edid.data)
+    return last
+
+  def DestroyEdid(self, edid_id):
+    """Destroys the internal record of EDID. The internal data will be freed.
+
+    Args:
+      edid_id: The ID of the EDID, which was created by CreateEdid().
+    """
+    if edid_id > 0:
+      self._all_edids[edid_id] = None
+    else:
+      raise DriverError('Not a valid edid_id.')
+
+  def ReadEdid(self, input_id):
+    """Reads the EDID content of the selected input on Chameleon.
+
+    Args:
+      input_id: The ID of the input connector.
+
+    Returns:
+      A byte array of EDID data, wrapped in a xmlrpclib.Binary object.
+    """
+    return xmlrpclib.Binary(self._input_flows[input_id].ReadEdid())
+
+  def _ApplyDefaultEdid(self):
+    """Applies the default EDID to all inputs."""
+    logging.info('Apply the default EDID to all inputs.')
+    for flow in self._input_flows.itervalues():
+      if flow:
+        flow.WriteEdid(self._all_edids[0])
+
+  def ApplyEdid(self, input_id, edid_id):
+    """Applies the EDID to the selected input.
+
+    Note that this method doesn't pulse the HPD line. Should call Plug(),
+    Unplug(), or FireHpdPulse() later.
+
+    Args:
+      input_id: The ID of the input connector.
+      edid_id: The ID of the EDID.
+    """
+    self._input_flows[input_id].WriteEdid(self._all_edids[edid_id])
+
+  def IsPlugged(self, input_id):
+    """Returns if the HPD line is plugged.
+
+    Args:
+      input_id: The ID of the input connector.
+
+    Returns:
+      True if the HPD line is plugged; otherwise, False.
+    """
+    return self._input_flows[input_id].IsPlugged()
+
+  def Plug(self, input_id):
+    """Asserts HPD line to high, emulating plug.
+
+    Args:
+      input_id: The ID of the input connector.
+    """
+    return self._input_flows[input_id].Plug()
+
+  def Unplug(self, input_id):
+    """Deasserts HPD line to low, emulating unplug.
+
+    Args:
+      input_id: The ID of the input connector.
+    """
+    return self._input_flows[input_id].Unplug()
+
+  def FireHpdPulse(self, input_id, deassert_interval_usec,
+                   assert_interval_usec=None, repeat_count=1):
+    """Fires a HPD pulse (high -> low -> high) or multiple HPD pulses.
+
+    Args:
+      input_id: The ID of the input connector.
+      deassert_interval_usec: The time in microsecond of the deassert pulse.
+      assert_interval_usec: The time in microsecond of the assert pulse.
+      repeat_count: The count of repeating the HPD pulses.
+    """
+    # TODO(waihong): Implement this method.
+    raise NotImplementedError('FireHpdPulse')
+
+  def _SelectInput(self, input_id):
+    """Selects the input on Chameleon.
+
+    Args:
+      input_id: The ID of the input connector.
+    """
+    if input_id != self._selected_input:
+      self._input_flows[input_id].Select()
+      self._selected_input = input_id
+
+  def DumpPixels(self, input_id, x=None, y=None, width=None, height=None):
+    """Dumps the raw pixel array of the selected area.
+
+    If not given the area, default to capture the whole screen.
+
+    Args:
+      input_id: The ID of the input connector.
+      x: The X position of the top-left corner.
+      y: The Y position of the top-left corner.
+      width: The width of the area.
+      height: The height of the area.
+
+    Returns:
+      A byte-array of the pixels, wrapped in a xmlrpclib.Binary object.
+    """
+    # TODO(waihong): Implement this method.
+    raise NotImplementedError('DumpPixels')
+
+  def ComputePixelChecksum(self, input_id, x=None, y=None, width=None,
+        height=None):
+    """Computes the checksum of pixels in the selected area.
+
+    If not given the area, default to compute the whole screen.
+
+    Args:
+      input_id: The ID of the input connector.
+      x: The X position of the top-left corner.
+      y: The Y position of the top-left corner.
+      width: The width of the area.
+      height: The height of the area.
+
+    Returns:
+      The checksum of the pixels.
+    """
+    # TODO(waihong): Implement this method.
+    raise NotImplementedError('ComputePixelChecksum')
+
+  def DetectResolution(self, input_id):
+    """Detects the source resolution.
+
+    Args:
+      input_id: The ID of the input connector.
+
+    Returns:
+      A (width, height) tuple.
+    """
+    self._SelectInput(input_id)
+    return self._input_flows[input_id].GetResolution()
