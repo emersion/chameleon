313a6ab51b857011f4abc2ec999f7a885340aee6
Joseph Hwang <josephsih@chromium.org>
2016-05-04 19:25:21 +0800

    Control bluetooth hid devices on Chameleon board
    This patch supports to control the bluetooth mouse emulated
    by an RN-42 kit. As we have already had bluetooth HID mouse
    object to control the kit, it would be cleaner to instantiate
    a bluetooth_mouse object in the fpga_tio driver. The
    bluetooth_hid_flow module is implemented for the control
    interface of the bluetooth mouse.
    
    BUG=chromium:610238
    TEST=Use the commands
    
    Change-Id: I4a6c5906dd2c7e928c7f7d7749de957b74ae0e03
    Reviewed-on: https://chromium-review.googlesource.com/343479
    Commit-Ready: Shyh-In Hwang <josephsih@chromium.org>
    Tested-by: Shyh-In Hwang <josephsih@chromium.org>
    Reviewed-by: Wai-Hong Tam <waihong@chromium.org>

--- chameleond/utils/ids.py
+++ chameleond/utils/ids.py
@@ -15,6 +15,11 @@ USB_AUDIO_IN = 8
 USB_AUDIO_OUT = 9
 USB_KEYBOARD = 10
 USB_TOUCH = 11
+BLUETOOTH_HID_KEYBOARD = 12
+BLUETOOTH_HID_GAMEPAD = 13
+BLUETOOTH_HID_MOUSE = 14
+BLUETOOTH_HID_COMBO = 15
+BLUETOOTH_HID_JOYSTICK = 16
 
 # Input/output ports
 INPUT_PORTS = [DP1, DP2, HDMI, VGA, MIC, LINEIN, USB_AUDIO_IN]
@@ -30,6 +35,13 @@ USB_AUDIO_PORTS = [USB_AUDIO_IN, USB_AUDIO_OUT]
 # Ports that support USB HID
 USB_HID_PORTS = [USB_KEYBOARD, USB_TOUCH]
 
+# Ports that support BLUETOOTH HID
+BLUETOOTH_HID_PORTS = [BLUETOOTH_HID_KEYBOARD,
+                       BLUETOOTH_HID_GAMEPAD,
+                       BLUETOOTH_HID_MOUSE,
+                       BLUETOOTH_HID_COMBO,
+                       BLUETOOTH_HID_JOYSTICK]
+
 # Convenience methods
 IsInputPort = lambda port_id: port_id in INPUT_PORTS
 IsOutputPort = lambda port_id: port_id in OUTPUT_PORTS
@@ -37,6 +49,7 @@ IsAudioPort = lambda port_id: port_id in AUDIO_PORTS
 IsVideoPort = lambda port_id: port_id in VIDEO_PORTS
 IsUSBAudioPort = lambda port_id: port_id in USB_AUDIO_PORTS
 IsUSBHIDPort = lambda port_id: port_id in USB_HID_PORTS
+IsBluetoothHIDPort = lambda port_id: port_id in BLUETOOTH_HID_PORTS
 
 # IDs of EDIDs
 EDID_ID_DEFAULT = 0
